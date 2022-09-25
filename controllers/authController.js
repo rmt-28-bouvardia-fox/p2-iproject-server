97e84af11cb7b63ca6adf2641f219f8fc5cb261d
LordFeexz <anandafiqri1403@gmail.com>
2022-09-25 21:57:16 +0700

    autj controller setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,35 @@
 class Controller {
 
     static async register(req,res,next){
-
+        try {
+            
+        } catch (err) {
+            
+        }
     }
 
     static async login(req,res,next){
-
+        try {
+            
+        } catch (err) {
+            
+        }
     }
 
     static async google(req,res,next){
-
+        try {
+            
+        } catch (err) {
+            
+        }
     }
 
     static async facebook(req,res,next){
-
+        try {
+            
+        } catch (err) {
+            
+        }
     }
 
 }
