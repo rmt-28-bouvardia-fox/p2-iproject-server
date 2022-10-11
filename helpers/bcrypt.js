24c79fc9db254751836c40b14820d73129cf36f9
Fahmi <af08juni@gmail.com>
2022-10-11 16:02:55 +0700

    create helpers/bcrypt.js for hashing password

---
+++ helpers/bcrypt.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcryptjs");
+const hash = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+const compare = (password, hashedPassword) => {
+  return bcrypt.compareSync(password, hashedPassword);
+};
+
+module.exports = { hash, compare };
