1fbdffc6fe1fbbd54673918ccc066614540657cb
pascal029 <pascalvirgiawan05@gmail.com>
2022-10-11 14:27:47 +0700

    add endpoint login

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -2,5 +2,6 @@ const ClientController = require('../Controllers/clientController')
 const router = require('express').Router()
 
 router.post('/register', ClientController.register)
+router.post('/login', ClientController.login)
 
 module.exports = router
