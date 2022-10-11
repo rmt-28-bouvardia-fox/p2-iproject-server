0816fc1162e69ad6edc642439b5dab9426e0a9f2
Fahmi <af08juni@gmail.com>
2022-10-11 16:59:41 +0700

    create folder routes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const exp = require("constants");
 const port = 3000;
+const cors = require("cors");
+const express = require("express");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(require("./routes"));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
