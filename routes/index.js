09fc62eaa369e9188b2e6da3cca6b6639bc18faa
Tri Buono Asto Nugroho <tribuonoasto@gmail.com>
2022-10-11 15:36:31 +0700

    Add authentication feature

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 "use strict";
 const express = require("express");
 const UserController = require("../controllers/userController");
+const authentication = require("../middlewares/authentication");
 const router = express.Router();
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+
+router.use(authentication);
 
 module.exports = router;
