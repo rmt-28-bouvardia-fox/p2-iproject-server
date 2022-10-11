c96cda7a2cee1cd9406a52d4e815ff767d66d404
Fahmi <af08juni@gmail.com>
2022-10-11 20:56:58 +0700

    create route for doctors, create route for register doctor, create seeder for doctors with hasing password

---
+++ routes/doctor.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const controllerDoctor = require("../controllers/controllerDoctor")
+
+router.post("/register", controllerDoctor.registerDoctor)
+// router.post("/login", controllerDoctor.loginDoctor)
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const routerPatient = require("./patient")
+const routerDoctor = require("./doctor")
 
 router.use("/patients", routerPatient)
+router.use("/doctors", routerDoctor)
 
 module.exports = router
