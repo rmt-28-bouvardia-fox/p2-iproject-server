94d69764f4d577c43e9ef1261d36bf67a3e9a992
LordFeexz <anandafiqri1403@gmail.com>
2022-10-11 12:05:50 +0700

    change to development

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,4 +1,16 @@
+const axios = require('axios')
+
 class Controller{
+
+    static async getMusic(req,res,next){
+        try {
+            const music = await axios({
+                url:``
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
 
 }
 
