f55c032cc91cf55e83deaf098b67d727d3958e83
P4rley <adwihersa@gmail.com>
2022-10-11 17:57:55 +0700

    Menambahkan username pada saat create user dan menghapus console.log di error handler

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -5,9 +5,9 @@ const { User } = require("../models");
 class UserController {
   static async registerUser(req, res, next) {
     try {
-      const { email, password } = req.body;
+      const { username, email, password } = req.body;
 
-      const user = await User.create({ email, password });
+      const user = await User.create({ username, email, password });
 
       res.status(201).json({ id: user.id, email: user.email });
     } catch (error) {
