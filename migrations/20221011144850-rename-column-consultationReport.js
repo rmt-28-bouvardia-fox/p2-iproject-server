0e83493cf5c21eeb00ad9b6e4a2470d26efc6796
Fahmi <af08juni@gmail.com>
2022-10-11 21:49:52 +0700

    fix rename column appointment id in consultation report

---
+++ migrations/20221011144850-rename-column-consultationReport.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.renameColumn(
+      "ConsultationReports",
+      "AppointmnetId",
+      "AppointmentId"
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.renameColumn(
+      "ConsultationReports",
+      "AppointmentId",
+      "AppointmnetId"
+    );
+  },
+};
