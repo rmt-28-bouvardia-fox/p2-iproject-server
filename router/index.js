213239316c33cf82e7f4706f812c295bd6317e47
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-10-12 01:09:36 +0700

    authentication done

--- router/index.js
+++ router/index.js
@@ -1,7 +1,12 @@
 const UserController = require('../controller/userController')
+const authentication = require('../middleware/authentication')
 
 const router = require('express').Router()
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+
+router.use(authentication)
+
+
 module.exports = router
