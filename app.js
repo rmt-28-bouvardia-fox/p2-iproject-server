d44935a5ca7df2cdd261788611855d8f0272efaf
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-10-12 01:03:44 +0700

    error handler done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const router = require('./router')
 const port = 3000
+const errorHandler = require('./middleware/errorhandler')
 
 app.use(cors())
 
@@ -10,6 +11,8 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use(router)
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log('litsen to port', port)
