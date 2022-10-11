a910cb15f3125b373901bc3570c94446456cd469
Tri Buono Asto Nugroho <tribuonoasto@gmail.com>
2022-10-11 15:16:37 +0700

    Add login feature
    Add login feature with access_token as successful login response

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,35 @@ class UserController {
         email,
         password,
       });
-      res.status(201).json({ message: `Have Fun ${user.username} !` });
+      res.status(201).json({ id: user.id, email: user.email });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async login(req, res, next) {
+    const { email, password } = req.body;
+    try {
+      if (!email || !password) {
+        throw { name: "invalid_credential" };
+      }
+
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        throw { name: "invalid_credential" };
+      }
+      const validatePassword = compareHash(password, user.password);
+
+      if (!validatePassword) {
+        throw { name: "invalid_credential" };
+      }
+
+      const payload = {
+        id: user.id,
+      };
+      const access_token = generateToken(payload);
+      res.status(200).json({ access_token });
     } catch (error) {
       next(error);
     }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const UserController = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 module.exports = router;
