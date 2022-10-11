1789f881a762c7d46f5f160e0681666faaa9e24d
elsaelsa95 <elsa.lustiana95@gmail.com>
2022-10-11 15:37:24 +0700

    update feature done

--- routes/routerInvitation.js
+++ routes/routerInvitation.js
@@ -1,7 +1,9 @@
-const router = require('express').Router()
-const Controller = require('./../controller/invitation');
+const router = require("express").Router();
+const authorization = require("../middleware/authorization");
+const Controller = require("./../controller/invitation");
 
-router.post('/create', Controller.createInvitation)
-router.get('/:coupleName', Controller.readInvitation)
+router.post("/create", Controller.createInvitation);
+router.get("/:coupleName", Controller.readInvitation);
+router.put("/:coupleName", authorization, Controller.updateInvitation);
 
-module.exports = router
+module.exports = router;
