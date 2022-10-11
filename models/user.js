64648e57ff612544744274df51c8f74e8d907a4f
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-10-11 22:43:58 +0700

    setup done

---
+++ models/user.js
@@ -0,0 +1,59 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Product, {as: 'OwnerProduct', foreignKey: "OwnerId"})
+      User.hasMany(models.Product, {as: 'BidderProduct',foreignKey: "BidderId"})
+      User.hasMany(models.UserProduct, {foreignKey: "UserId"})
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {msg: "username required"},
+        notNull: {msg: "username required"}
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {msg: "email must be unique"},
+      validate: {
+        notEmpty: {msg: "email required"},
+        notNull: {msg: "email required"},
+        isEmail: {msg: "invalid email format"}
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {msg: "password required"},
+        notNull: {msg: "password required"}
+      }
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {msg: "role required"},
+        notNull: {msg: "role required"}
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
---
+++ models/userproduct.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      UserProduct.belongsTo(models.User, {foreignKey: "UserId"})
+      UserProduct.belongsTo(models.Product, {foreignKey: "ProductId"})
+    }
+  }
+  UserProduct.init({
+    UserId: DataTypes.INTEGER,
+    ProductId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'UserProduct',
+  });
+  return UserProduct;
+};
