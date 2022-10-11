3b0325ee918aa1d0834021802c841bbe8e0ab38f
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-10-12 02:58:24 +0700

    authorization done

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,29 +1,29 @@
-const jwt = require('jsonwebtoken')
-const { User } = require(`../models`)
-const SECRET = "rahasia"
+const jwt = require("jsonwebtoken");
+const { User } = require(`../models`);
+const SECRET = "rahasia";
 
 const authentication = async (req, res, next) => {
-    try {
-        const { access_token } = req.headers
-        if(!access_token){
-            throw {name: "invalid_token"}
-        }
-        const payload = jwt.verify(access_token, SECRET)
-        const user = await User.findByPk(payload.id)
-        if(!user){
-            throw {name: "invalid_token"}
-        }
-
-        req.user = {
-            id: user.id,
-            role: user.role,
-            username: user.username
-        }
-
-        next()
-    } catch (error) {
-        next(error)
+  try {
+    const { access_token } = req.headers;
+    if (!access_token) {
+      throw { name: "invalid_token" };
     }
-}
+    const payload = jwt.verify(access_token, SECRET);
+    const user = await User.findByPk(payload.id);
+    if (!user) {
+      throw { name: "invalid_token" };
+    }
 
-module.exports = authentication
+    req.user = {
+      id: user.id,
+      role: user.role,
+      username: user.username,
+    };
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = authentication;
