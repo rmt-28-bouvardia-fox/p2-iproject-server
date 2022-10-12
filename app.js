45c84bc212c750141b435b99b810d068b19853b4
pascal029 <pascalvirgiawan05@gmail.com>
2022-10-13 06:41:57 +0700

    initiate deployment, change config production, change port, change .env requirement, add file Procfile

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-require('dotenv').config()
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config() 
+ }
 
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 const router = require('./routes')
 const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
