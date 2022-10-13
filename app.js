07c40b9a2741542dfc34345a8d1c7274a8f636b5
Tri Buono Asto Nugroho <tribuonoasto@gmail.com>
2022-10-13 07:20:03 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const router = require("./routes");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json());
